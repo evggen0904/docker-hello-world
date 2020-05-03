1a4ad01a81335325534b3b5c72e558fd06ffeb60
goryainoves <Goryainov.E.S@sberbank.ru>
2020-05-03 14:25:20 +0300

    init commit

---
+++ src/main/java/com/mycompany/docker/hello/world/rest/DockerRest.java
@@ -0,0 +1,13 @@
+package com.mycompany.docker.hello.world.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerRest {
+
+    @GetMapping("/")
+    public String getHello() {
+        return "Hello docker";
+    }
+}
---
+++ src/test/java/com/mycompany/docker/hello/world/DockerHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.docker.hello.world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerHelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
